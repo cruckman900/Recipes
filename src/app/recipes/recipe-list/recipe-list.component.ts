2dc850c4aaf152edd4e538dc4221a1b0a2e4469e
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2024-04-04 08:08:09 -0400

    added models and layout structure for components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://reusegrowenjoy.com/wp-content/uploads/Chickn-and-yellow-rice-in-the-pot.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://reusegrowenjoy.com/wp-content/uploads/Chickn-and-yellow-rice-in-the-pot.jpg')
+  ];
 }
